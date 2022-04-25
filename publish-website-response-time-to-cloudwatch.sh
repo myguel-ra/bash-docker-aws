d62126a2246a01f4d378302468c53863184feed6
Miguel Almeida <myguel.ra@gmail.com>
2022-04-25 23:05:54 +0100

    Delete publish-website-response-time-to-cloudwatch.sh

--- publish-website-response-time-to-cloudwatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PageLoadTime=$(curl www.google.com --compressed -s -o /dev/null -w  "%{time_starttransfer}\n")
-echo "$PageLoadTime"
-
-aws cloudwatch put-metric-data --metric-name PageLoadTime --namespace MyService --value "$PageLoadTime"
-echo "Published metric to AWS"
